8620d0dd888e747c9e8fe321b54d13cf5b0a7d32
Elsayed Abdullah <sayedaladway@gmail.com>
2019-10-23 20:35:31 +0200

    1st edition

---
+++ dist/js/main.js
@@ -0,0 +1,53 @@
+$(function () {
+
+  // scroll to top
+  $(window).scroll(function () {
+    var windowScroll = $(window).scrollTop();
+
+    if (windowScroll >= 600) {
+      $('.scroll-top').fadeIn();
+    } else {
+      $('.scroll-top').fadeOut(100);
+    }
+  });
+
+  $(".scroll-top").click(function () {
+    $("html, body").animate({
+        scrollTop: 0
+      },
+      600
+    );
+  });
+
+
+
+  $("#convertToForm").click(function () {
+    $(".instadiet-analysis .front").css({
+      "z-index": 0,
+      "transform": "rotateY(180deg)"
+    });
+    $(".instadiet-analysis .back").css("transform", "rotateY(0)");
+
+  });
+
+
+  // burgor button change shape to x button
+  $(".the-button").on("click", function () {
+    $("html,body").toggleClass("overlay");
+    $(this).toggleClass("transformed");
+    $(".navbar-collapse").toggleClass("show");
+  });
+
+  $("body").on("click", function (e) {
+    var $currEl = $(e.currentTarget);
+    if (!$currEl.is(".navbar") && !$currEl.closest(".navbar").length) {
+      $("html,body").removeClass("overlay");
+      $(".navbar-collapse").removeClass("show");
+      $(".the-button").removeClass("transformed");
+    }
+  });
+  $(".navbar").on("click", function (e) {
+    e.stopPropagation();
+  });
+
+});
